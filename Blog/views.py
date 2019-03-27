1f29302aa9d471d8cc6f86515a8e6d30c0bccc26
MohammadReza <sh.mohammad66@gmail.com>
2019-03-27 23:45:05 +0430

    create first view

--- Blog/views.py
+++ Blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("salam aleykom. this is first view for Blog app")
