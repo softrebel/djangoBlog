87b6e0eb88f7f06c53be758f0dc2700c3ff5f426
MohammadReza <sh.mohammad66@gmail.com>
2019-03-30 23:51:01 +0430

    add view & template for show post basically

--- Blog/views.py
+++ Blog/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from djangoBlog.settings import BASE_DIR
-
+from Blog.models import Post
+import json
 
 def index(request):
+
     return HttpResponse("salam aleykom. this is first view for Blog app")
 
 def about(request):
     return HttpResponse("kari az kusha")
+
+def posts(request):
+    list = Post.objects.all().order_by('-id')[:2]
+    return render(request, 'post.html', {'posts': list})
